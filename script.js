3f8933b464438c4e8f90ff72747ddebb99178356
Kaandalgar <77542177+Kaandalgar@users.noreply.github.com>
2025-04-06 17:59:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function showPopup() {
+    document.getElementById("discount-code").innerText = "Congratulations! Here is your 10% discount code:";
+    document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+
